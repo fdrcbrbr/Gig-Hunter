a828f65476e0571c68557063145bf61fbc618f10
fdrcbrbr <fefo.barberi@gmail.com>
2025-10-08 20:35:48 +0200

    header first step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import EventList from "@/components/eventList";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <div >
+      <Header/>
       <EventList/>
     </div>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+//import Link from "next/link";
+//import { Search, ShoppingCart, CircleUserRound } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="flex w-full">
+      <div className="flex-1 flex justify-start items-center">
+        <h1 className="--font-bitcount font-bold">Gig Hunter</h1>
+      </div>
+    </header>
+  );
+}
