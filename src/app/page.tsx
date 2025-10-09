10a8f2ba48bcbd2405823d11defe057d42416483
fdrcbrbr <fefo.barberi@gmail.com>
2025-10-09 12:54:28 +0200

    Need to instal a package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import EventList from "@/components/eventList";
-import Header from "@/components/header";
 
 export default function Home() {
   return (
     <div >
-      <Header/>
       <EventList/>
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 
 export default function Header() {
   return (
-    <header className="flex w-full bg-[url('/public/bg-header.jpg')]">
+    <header className="flex w-full h-fit bg-[url('(/images/bg-header.jpg')]">
       <div className="flex-1 flex justify-start items-center">
         <h1 className="--font-poppins font-bold">Gig Hunter</h1>
       </div>
