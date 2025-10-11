7862c9de6c0784db56f3abe1593229b91f9ceb96
fdrcbrbr <fefo.barberi@gmail.com>
2025-10-11 15:02:26 +0200

    Saving last about hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,15 @@
+import SearchBar from "./ui/search";
 
-// components/Hero.jsx
 export default function Hero() {
   return (
     <div className="w-full min-h-[70vh] p-4 flex flex-col items-center justify-center text-center">
-      <h1 className="text-[var(--color-cream)] text-5xl font-bold mb-4">
+      <h2 className="text-[var(--color-cream)] text-5xl font-bold mb-4">
         Find Your Next Gig
-      </h1>
+      </h2>
       <p className="text-[var(--color-cream)] text-xl max-w-2xl">
         Explore live music events in your city. From indie gigs to big festivals, we&apos;ve got you covered.
       </p>
+      <SearchBar/>
     </div>
   );
 }
