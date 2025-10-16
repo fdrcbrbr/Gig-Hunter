b749700a5e80f57f8c327cd1aad73cb002fdfe7f
fdrcbrbr <fefo.barberi@gmail.com>
2025-10-16 15:15:23 +0200

    Saving some changes

--- src/components/ui/filter-genre.tsx
+++ src/components/ui/filter-genre.tsx
@@ -1,32 +1,35 @@
 "use client";
-
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { GENRE_DISPLAY_NAMES } from "@/data/consts"; 
+import { GENRE_DISPLAY_NAMES } from "@/data/consts";
+import * as React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function FilterGenre() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
-
   const currentGenreCode = searchParams.get("genre") || "";
-
 
   const handleFilterChange = (value: string) => {
     const params = new URLSearchParams(searchParams);
-
-    if (value) {
+    if (value && value !== "all") {
       params.set("genre", value);
     } else {
       params.delete("genre");
     }
-
     replace(`${pathname}?${params.toString()}`);
   };
 
-
   const genreOptions = [
-    { name: "All Genres", code: "" },
+    { name: "All Genres", code: "all" },
     ...Object.entries(GENRE_DISPLAY_NAMES).map(([name, code]) => ({
       name,
       code,
@@ -34,21 +37,22 @@ export default function FilterGenre() {
   ];
 
   return (
-    <div className="flex gap-4 p-4 bg-neutral-100 rounded-lg">
-      <div className="flex flex-col">
-        <span className="font-medium mb-2"> Genre</span>
-        <select
-          value={currentGenreCode}
-          onChange={(e) => handleFilterChange(e.target.value)}
-          className="px-4 py-2 rounded-full border border-gray-300 bg-white"
-        >
-          {genreOptions.map((option) => (
-            <option key={option.code} value={option.code}>
-              {option.name}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div className="flex bg-neutral-100 rounded-lg">
+      <Select value={currentGenreCode} onValueChange={handleFilterChange}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a genre" />
+        </SelectTrigger>
+        <SelectContent className="max-h-[200px] overflow-y-auto">
+          <SelectGroup>
+            <SelectLabel>Genres</SelectLabel>
+            {genreOptions.map((option) => (
+              <SelectItem key={option.code} value={option.code}>
+                {option.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 }
